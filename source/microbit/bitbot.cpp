73a5ef40a846b7472fafc62425d88cc92f26e0ba
Mathis Gerdes <MathisGerdes@gmail.com>
2017-06-29 21:55:47 +0200

    Full motor implementation (works only without script).

--- source/microbit/bitbot.cpp
+++ source/microbit/bitbot.cpp
@@ -91,61 +91,47 @@ MP_DEFINE_CONST_FUN_OBJ_0(bitbot_get_brightness_obj, bitbot_get_brightness);
 
 STATIC mp_obj_t bitbot_set_speed(mp_uint_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args)
 {
-  float speed = 60.0;
-  int speed_exact;
-  if (speed < 0) {
-    speed_exact = 1023 * (1 + speed / 100.0);
-    // microbit_pin_write_digital(PIN_MOTOR_LEFT_DIR, mp_const_int_one);
-  } else {
-    speed_exact = 1023 * (speed / 100.0);
-    // microbit_pin_write_digital((microbit_pin_obj_t*)&microbit_p8_obj, mp_const_int_zero);
-    microbit_pin_write_digital(PIN_BUZZER, mp_const_int_one);
-    microbit_sleep(mp_obj_new_int(100));
-    microbit_pin_write_digital(PIN_BUZZER, mp_const_int_zero);
-  }
-  // microbit_pin_write_analog(PIN_MOTOR_LEFT_SPEED, mp_obj_new_int(speed_exact));
-  return mp_const_none;
+  static const mp_arg_t allowed_args[] = {
+      { MP_QSTR_a,    MP_ARG_OBJ, {.u_obj = mp_const_none } },
+      { MP_QSTR_b,    MP_ARG_OBJ, {.u_obj = mp_const_none } },
+      { MP_QSTR_left, MP_ARG_KW_ONLY | MP_ARG_OBJ, {.u_obj = mp_const_none} },
+      { MP_QSTR_right, MP_ARG_KW_ONLY | MP_ARG_OBJ, {.u_obj = mp_const_none} },
+  };
 
-  // static const mp_arg_t allowed_args[] = {
-  //     { MP_QSTR_a,    MP_ARG_OBJ, {.u_obj = mp_const_none } },
-  //     { MP_QSTR_b,    MP_ARG_OBJ, {.u_obj = mp_const_none } },
-  //     { MP_QSTR_left, MP_ARG_KW_ONLY | MP_ARG_OBJ, {.u_obj = mp_const_none} },
-  //     { MP_QSTR_right, MP_ARG_KW_ONLY | MP_ARG_OBJ, {.u_obj = mp_const_none} },
-  // };
-  //
-  // // parse args
-  // mp_arg_val_t args[MP_ARRAY_SIZE(allowed_args)];
-  // mp_arg_parse_all(n_args, pos_args, kw_args, MP_ARRAY_SIZE(allowed_args), allowed_args, args);
-  //
-  // bool at_least_one_arg = false;
-  // switch (n_args) {
-  //   case 0:
-  //     if (args[2].u_obj != mp_const_none) {
-  //       setPercent(PIN_MOTOR_LEFT_DIR, PIN_MOTOR_LEFT_SPEED, mp_obj_get_float(args[2].u_obj));
-  //       at_least_one_arg = true;
-  //     }
-  //     if (args[3].u_obj != mp_const_none) {
-  //       setPercent(PIN_MOTOR_RIGHT_DIR, PIN_MOTOR_RIGHT_SPEED, mp_obj_get_float(args[3].u_obj));
-  //       at_least_one_arg = true;
-  //     }
-  //     if (!at_least_one_arg) {
-  //       nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_TypeError,
-  //                 "function expects 1 or 2 positional arguments OR keyword arguments 'left', 'right'"));
-  //     }
-  //     return mp_const_none;
-  //   case 1:
-  //     setPercent(PIN_MOTOR_LEFT_DIR, PIN_MOTOR_LEFT_SPEED, mp_obj_get_float(args[0].u_obj));
-  //     setPercent(PIN_MOTOR_RIGHT_DIR, PIN_MOTOR_RIGHT_SPEED, mp_obj_get_float(args[0].u_obj));
-  //     break;
-  //   case 2:
-  //     setPercent(PIN_MOTOR_LEFT_DIR, PIN_MOTOR_LEFT_SPEED, mp_obj_get_float(args[0].u_obj));
-  //     setPercent(PIN_MOTOR_RIGHT_DIR, PIN_MOTOR_RIGHT_SPEED, mp_obj_get_float(args[1].u_obj));
-  // }
-  // // check that not both positional and keyword parameter was given
-  // if (args[2].u_obj != mp_const_none || args[3].u_obj != mp_const_none) {
-  //   nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_TypeError,
-  //             "function expects 1 or 2 positional arguments OR keyword arguments 'left', 'right'"));
-  // }
+  // parse args
+  mp_arg_val_t args[MP_ARRAY_SIZE(allowed_args)];
+  mp_arg_parse_all(n_args, pos_args, kw_args, MP_ARRAY_SIZE(allowed_args), allowed_args, args);
+
+  bool at_least_one_arg = false;
+  switch (n_args) {
+    case 0:
+      if (args[2].u_obj != mp_const_none) {
+        setPercent(PIN_MOTOR_LEFT_DIR, PIN_MOTOR_LEFT_SPEED, mp_obj_get_float(args[2].u_obj));
+        at_least_one_arg = true;
+      }
+      if (args[3].u_obj != mp_const_none) {
+        setPercent(PIN_MOTOR_RIGHT_DIR, PIN_MOTOR_RIGHT_SPEED, mp_obj_get_float(args[3].u_obj));
+        at_least_one_arg = true;
+      }
+      if (!at_least_one_arg) {
+        nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_TypeError,
+                  "function expects 1 or 2 positional arguments OR keyword arguments 'left', 'right'"));
+      }
+      return mp_const_none;
+    case 1:
+      setPercent(PIN_MOTOR_LEFT_DIR, PIN_MOTOR_LEFT_SPEED, mp_obj_get_float(args[0].u_obj));
+      setPercent(PIN_MOTOR_RIGHT_DIR, PIN_MOTOR_RIGHT_SPEED, mp_obj_get_float(args[0].u_obj));
+      break;
+    case 2:
+      setPercent(PIN_MOTOR_LEFT_DIR, PIN_MOTOR_LEFT_SPEED, mp_obj_get_float(args[0].u_obj));
+      setPercent(PIN_MOTOR_RIGHT_DIR, PIN_MOTOR_RIGHT_SPEED, mp_obj_get_float(args[1].u_obj));
+  }
+  // check that not both positional and keyword parameter was given
+  if (args[2].u_obj != mp_const_none || args[3].u_obj != mp_const_none) {
+    nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_TypeError,
+              "function expects 1 or 2 positional arguments OR keyword arguments 'left', 'right'"));
+  }
+  return mp_const_none;
 }
 MP_DEFINE_CONST_FUN_OBJ_KW(bitbot_set_speed_obj, 0, bitbot_set_speed);
 
